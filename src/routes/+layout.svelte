e8919630260e606538c7bded88679b805b417e4b
elizabeth zhang <elizabeth.zhangey@gmail.com>
2023-09-09 21:42:53 -0400

    fix: static site generation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,30 +1,5 @@
-
 <script lang="ts">
-    import '../global.css'
+    '../global.css'
+</script> 
 
-    export const prerender = true;
-</script>
 <slot />
-
-<style>
-
-    nav {
-        display: flex;
-        justify-content: flex-end;
-        gap: 1rem;
-        padding: 2rem 5rem 0;
-
-    }
-
-    nav > a {
-        color: var(--indigo-base);
-        font-family: "Roboto Slab";
-        font-size: 1.25rem;
-        font-weight: bold;
-        text-decoration: none;
-    }
-
-    a.active, a:hover  {
-        text-decoration: underline;
-    }
-</style>
